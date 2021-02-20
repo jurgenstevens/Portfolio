6154232d9f9fd48a55374f6f2c340a1d5a35bf21
Jurgen <jurgen.stevens@yahoo.com>
2021-02-19 21:43:18 -0600

    Set divs up for what portfolio will consist of.

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <div>
+      Header
+    </div>
+    <div>
+      Bio
+    </div>
+    <div>Projects</div>
+    <div>Footer</div>
+  </div>
 }

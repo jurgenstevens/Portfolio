71fbe83763c8d97e4b702f52503dfe383ce0192b
jurgenstevens <jurgen.stevens@yahoo.com>
2021-07-14 02:00:21 -0500

    Created component JS files.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello from the Header!</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const sidebar = () => {
+    return (
+        <div>
+            <h1>Hello from the Sidebar!</h1>
+        </div>
+    )
+}
+
+export default sidebar

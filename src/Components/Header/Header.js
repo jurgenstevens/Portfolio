c0070c416ba18618c93aac7ebfd7195bf8a4e26e
jurgenstevens <jurgen.stevens@yahoo.com>
2021-07-19 02:14:33 -0500

    Created a greet with Date and getHours method in Header component.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,23 @@ import React from 'react'
 import "./Header.css"
 
 const Header = () => {
+    // Set up day time greet REF: https://stackoverflow.com/questions/13244939/javascript-to-output-text-based-on-users-current-time
+    let today = new Date()
+    let currentHour = today.getHours()
+    let greet
+
+    if (currentHour < 12) {
+        greet = <h1>Good morning!</h1>
+    } else if (currentHour < 18) {
+        greet = <h1>Good afternoon!</h1>
+    } else {
+        greet = <h1>Good evening.</h1>
+    }
+
+
     return (
         <div className="headerDiv">
-            <h1 className="headerGreet" >Good morning!</h1>
+            {greet}
         </div>
     )
 }

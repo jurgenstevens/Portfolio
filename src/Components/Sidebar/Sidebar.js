057a21156f9d8006b46952216dd2c0c310dc020c
jurgenstevens <jurgen.stevens@yahoo.com>
2021-07-18 22:22:45 -0500

    Aligned components in the positions needed.

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Sidebar.css"
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
     return (
-        <h1>Hello from Sidebar</h1>
+    <div className="sidebarDiv">
+        <Link className="links" to="/">Home</Link>
+        <Link className="links" to="/about">About Me</Link>
+        <Link className="links" to="/projects">Projects</Link>
+        <Link className="links" to="/contact">Contact Me</Link>
+        <Link className="links" to="/resume">Resume</Link>
+    </div>
     )
 }
 

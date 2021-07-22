a1874d9eba48bfd7d14af08b903a7ab3e2b0b8c5
jurgenstevens <jurgen.stevens@yahoo.com>
2021-07-21 23:56:06 -0500

    Created Projects css and js files.

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Sidebar.css"
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 const Sidebar = ({projects}) => {
@@ -9,13 +9,14 @@ const Sidebar = ({projects}) => {
     <div className="sidebarDiv">
         <Link className="links" id="homeLink" to="/">Home</Link>
         <hr />
-        <Link className="links" to="/about">About Me</Link>
+        <Link className="links" to="/about" >About Me</Link>
         <hr />
-        <Link className="links" to="/projects">Projects</Link>
+        <Link className="links" to="/projects" render={() => <Projects />}>Projects</Link>
         <hr />
         <Link className="links" to="/contact">Contact Me</Link>
         <hr />
         <Link className="links" to="/resume">Resume</Link>
+        
     </div>
     )
 }

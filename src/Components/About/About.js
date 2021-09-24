84a53f726839c37cb245172abdadf66d79e184b8
jurgenstevens <jurgen.stevens@yahoo.com>
2021-09-24 17:34:00 -0500

    Language cards set with image, title and background color.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,16 +6,16 @@ const About = () => {
     // console.log(location.state.languages)
     const languages = location.state.languages.map((language) => 
             {return <div className="card">
-                    <img src={language.image} alt="language-pic" />
-                        <h2 className="projectTitle"><span>{language.name}</span></h2>
+                        <img src={language.image} className="skillCover" alt="language-pic" />
+                        <h2 className="skillName"><span>{language.name}</span></h2>
                     </div>
             }
         )
 
     return (
         <div className="aboutDiv">
-            <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together.</h1>
-            <div>
+            <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Below are this skills I have to offer.</h1>
+            <div className="skills">
                 {languages}
             </div>
         </div>

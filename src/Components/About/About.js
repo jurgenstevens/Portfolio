e125a6b6d2c9e5c3642a4bda75b371a7ab2d25f3
jurgenstevens <jurgen.stevens@yahoo.com>
2021-09-25 03:05:30 -0500

    Add space around languages rendered.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,16 +5,17 @@ const About = () => {
     const location = useLocation()
     // console.log(location.state.languages)
     const languages = location.state.languages.map((language) => 
-            {return <div className="card">
-                        <img src={language.image} className="skillCover" alt="language-pic" />
-                        <h2 className="skillName"><span>{language.name}</span></h2>
-                    </div>
-            }
-        )
+        {return <div className="aboutCard">
+                    <img className="skillCover" src={language.image} alt="language-pic" />
+                    <h2 className="skillName">{language.name}</h2>
+                </div>
+        })
 
     return (
         <div className="aboutDiv">
+            <div className="bio">
             <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Below are this skills I have to offer.</h1>
+            </div>
             <div className="skills">
                 {languages}
             </div>

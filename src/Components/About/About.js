fd29dce0d0f8873ddb73f40529b46c12221f9c1a
jurgenstevens <jurgen.stevens@yahoo.com>
2021-09-25 23:23:30 -0500

    Linked and set all clard classes for all components to have access.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,19 +6,20 @@ const About = () => {
     // console.log(location.state.languages)
     const languages = location.state.languages.map((language) => 
         {return <div className="aboutCard">
-                    <img className="skillCover" src={language.image} alt="language-pic" />
-                    <h2 className="skillName">{language.name}</h2>
+                    <a className="cardCover" target="_blank" rel="noreferrer" href={language.link}>
+                        <h4 className="cardName">{language.name}</h4>
+                        <h4 className="cardCategory">{language.category}</h4>
+                    </a>
+                    <h2 className="cardTitle"><span>{language.name}</span></h2>
                 </div>
         })
 
     return (
         <div className="aboutDiv">
-            <div className="bio">
-            <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Below are this skills I have to offer.</h1>
-            </div>
-            <div className="skills">
+            {/* <div className="bio">
+                <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Below are this skills I have to offer.</h1>
+            </div> */}
                 {languages}
-            </div>
         </div>
     )
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-// import myImage from '../../../public/Images/hillbomber.png'
-
 
 const Projects = () => {
     // this will bring in the state from the Link in Main.js containing the props
@@ -9,15 +7,14 @@ const Projects = () => {
     console.log(location.state.projects)
     const projectList = location.state.projects.map((project) => 
             {return <div className="card">
-                        <a className="imageCover" target="_blank" rel="noreferrer" href={project.link}>
-                            <h4 className="projectName">{project.name}</h4>
-                            <h4 className="projectCategory">{project.category}</h4>
+                        <a className="cardCover" target="_blank" rel="noreferrer" href={project.link}>
+                            <h4 className="cardName">{project.name}</h4>
+                            <h4 className="cardCategory">{project.category}</h4>
                         </a>
-                        <h2 className="projectTitle"><span>{project.name}</span></h2>
-                        <p className="projectAbout">{project.about}</p>
+                        <h2 className="cardTitle"><span>{project.name}</span></h2>
+                        <p className="cardAbout">{project.about}</p>
                     </div>
-            }
-        )
+            })
     return (
         <div className="projectsDiv">
             {projectList}

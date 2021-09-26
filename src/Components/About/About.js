a1464226e1ea3e1a1c61b8e8248cd45898bd71c7
jurgenstevens <jurgen.stevens@yahoo.com>
2021-09-26 02:02:57 -0500

    Set language and framework cards in about me.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,22 +4,32 @@ import { useLocation } from 'react-router-dom'
 const About = () => {
     const location = useLocation()
     // console.log(location.state.languages)
+    // console.log(location.state.frameworks)
     const languages = location.state.languages.map((language) => 
-        {return <div className="aboutCard">
-                    <a className="cardCover" target="_blank" rel="noreferrer" href={language.link}>
-                        <h4 className="cardName">{language.name}</h4>
-                        <h4 className="cardCategory">{language.category}</h4>
-                    </a>
-                    <h2 className="cardTitle"><span>{language.name}</span></h2>
+        {return <div className="aboutCover">
+                    <div className="aboutText">
+                        <h4 className="aboutTitle">{language.name}</h4>
+                        <h4 className="aboutCategory">{language.category}</h4>
+                    </div>
+                    <h4 className="aboutName"><span>{language.name}</span></h4>
                 </div>
         })
-
+    const frameworks = location.state.frameworks.map((framework) => 
+    {return <div className="aboutCover">
+                <div className="aboutText">
+                    <h4 className="aboutTitle">{framework.name}</h4>
+                    <h4 className="aboutCategory">{framework.category}</h4>
+                </div>
+                <h4 className="aboutName"><span>{framework.name}</span></h4>
+            </div>
+    })
     return (
         <div className="aboutDiv">
-            {/* <div className="bio">
-                <h1>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Below are this skills I have to offer.</h1>
-            </div> */}
+            <div className="bio">
+                <h3>I'm a fullstack web developer from Chicago, Illinois, USA and I love building things that bring people closer together. Besides being fluent in Spanish, and knowing some Italian and Bosnian, below are this skills I have to offer.</h3>
+            </div>
                 {languages}
+                {frameworks}
         </div>
     )
 }

954e027d80175f346720d6a8bb535ebe2e058fe4
jurgenstevens <jurgen.stevens@yahoo.com>
2022-05-06 00:57:00 -0500

    Change home bio.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,9 +7,9 @@ export const Home = () => {
             <h1 className="profileName">Jurgen L. Stevens</h1>
             <h5 className="profileInfo">Software Developer | Skateboarder | Chicagoan</h5>
         </div>
-        <h3>What a priviledge to have the knowledge and power of software engineering to create apps, sites, and tools to help this world become a better place and for humanity to excel.</h3>
+        <h3>Chicago-based Full Stack Software Developer and General Assembly Instructional Associate experienced in technologies such as JavaScript, Python, React.js, Express.js, MongoDB, Node.js and Flask.</h3>
         <h2>
-            My name is Jurgen! Have a look around!
+            Have a look around!
         </h2>
     </div>
     )
